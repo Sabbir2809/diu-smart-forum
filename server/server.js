195e781fd4ffd48eefa05fc342a2a2cb617c7026
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-06-24 01:12:01 +0600

    Bug Fix

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 // Dependencies
 const app = require('./app');
-const { connectDB } = require('./config/db');
+const { connectDB } = require('./config/connectDB');
 
 // PORT
 const port = process.env.PORT || 5000;
