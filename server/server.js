2726fefcb94b1595e7de78a1e8cbba32aae982d6
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-06-24 01:04:26 +0600

    MongoDB Database Related Problem

--- server/server.js
+++ server/server.js
@@ -2,11 +2,10 @@
 const app = require('./app');
 const { connectDB } = require('./config/db');
 
-// MongoDB Connection
-connectDB();
-
 // PORT
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+  // MongoDB Connection
+  connectDB();
 });
